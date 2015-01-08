821c4a5d77e00da2e5c4fcf3422445e9125dabfc
jayvachon <jaylvachon@gmail.com>
2015-01-08 15:00:37 -0500

    update bean pot

---
+++ Assets/Scripts/Equipment/BeanPot.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeanPot : IBeanPool {
+
+	// The BeanPot is the pool that players throw beans into
+	// So every player has their own bean pool, but there's only one pot
+
+	int beanCount = 0;
+	public int BeanCount {
+		get { return beanCount; }
+	}
+
+	public void OnRoundStart (bool isDecider=false) {
+		beanCount = BeanValues.roundPot;
+	}
+
+	public void Empty () {
+		SetBeanCount (0);
+	}
+
+	public void SetBeanCount (int count) {
+		beanCount = count;
+		SendUpdateMessage ();
+	}
+
+	public void AddBeans (int amount) {
+		beanCount += amount;
+		SendUpdateMessage ();
+	}
+
+	public bool SubtractBeans (int amount) {
+		if (amount > beanCount) 
+			return false;
+		beanCount -= amount;
+		SendUpdateMessage ();
+		return true;
+	}
+
+	void SendUpdateMessage () {
+		Events.instance.Raise (new UpdateBeanPotEvent (beanCount));
+	}
+}
