c49e4a066bdaa4020c443de269821832be52ad42
jayvachon <jaylvachon@gmail.com>
2015-05-05 17:30:15 -0400

    add force disconnect event & inactive button sprite

---
+++ Assets/Scripts/EventHandling/Events/Network/ForceDisconnectEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceDisconnectEvent : GameEvent {
+
+	public readonly bool wasHost = false;
+	
+	public ForceDisconnectEvent (bool wasHost) {
+		this.wasHost = wasHost;
+	} 
+}
